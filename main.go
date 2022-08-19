4e13532ea941acf5fd79bb4f6c4d5d5377ef8c48
avocadojesus <fred@platejoy.com>
2022-08-19 12:48:04 -0700

    add main.go

---
+++ main.go
@@ -0,0 +1,8 @@
+package api_contract_go
+
+import (
+  "api-contract-go/cmd/api_contract"
+)
+
+var ExpectValidPayload = api_contract.ExpectValidPayload
+var ValidatePayload = api_contract.ValidatePayload
