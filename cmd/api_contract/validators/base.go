0bcf6e21500e3e6a3d2690879b6bc59d7fb22ae4
avocadojesus <fred@platejoy.com>
2022-08-24 10:28:36 -0700

    move validate param logic back to base package

--- cmd/api_contract/validators/base.go
+++ cmd/api_contract/validators/base.go
@@ -2,7 +2,11 @@ package validators
 
 import (
   "reflect"
+  "time"
 )
+
+const DATE_FORMAT = "2006-01-02"
+const DATETIME_FORMAT = time.RFC3339
 
 func CheckArrayForType(arr []interface{}, expectedType string) bool {
   for _, item := range arr {
